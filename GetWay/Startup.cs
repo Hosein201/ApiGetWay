100cdf8543c17edeece1b6b713e6a459e9dd177e
Hossein Golmohammadi <42300369+Hosein201@users.noreply.github.com>
2021-11-12 20:38:10 +0330

    Add Ocelot

--- GetWay/Startup.cs
+++ GetWay/Startup.cs
@@ -1,21 +1,28 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace OrderProcessing
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +34,7 @@ namespace OrderProcessing
             }
 
             app.UseRouting();
+            app.UseOcelot();
 
             app.UseEndpoints(endpoints =>
             {
